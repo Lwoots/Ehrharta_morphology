95c62cb0f5523df9bf59e1e38f7eda1cff64f6a9
Lara Wootton <larawootton@gmail.com>
2020-01-20 14:02:26 +0200

    Commit before major revamp

--- src/Data_cleaning.R
+++ src/Data_cleaning.R
@@ -5,7 +5,7 @@
 if(!require(pacman)){install.packages("pacman", dependencies=TRUE); library(pacman)}
 p_load(here, tidyverse, visdat, ggfortify, corrplot)
 
-dat <- read.csv(here("Raw/Unprocessed", "Morphology_20191003.csv"), sep = ";")
+dat <- read.csv(here("Raw/Unprocessed", "Morphology_20200114.csv"), sep = ";")
 
 #Data exploration
 head(dat)
@@ -15,7 +15,7 @@ glimpse(dat)
 
 pairs(dat[,4:21])
 
-#dat_omit <- na.omit(dat)
+dat_omit <- na.omit(dat[,c(1:24)])
 #
 #corr_dat <- cor(dat[,4:24])
 #corrplot(corr_dat[,4:21])
@@ -25,7 +25,7 @@ pairs(dat[,4:21])
 #hist((dat$Plant_height-dat$Veg_height)/dat$Plant_height)
 
 
-datforpca <- scale(dat[,c(4:24)])
+datforpca <- scale(dat_omit[,c(4:24)])
 
 hist(dat$Plant_height)
 
@@ -34,14 +34,14 @@ dat_pca <- prcomp(datforpca)
 plot(dat_pca$x[,1], dat_pca$x[,2])
 
 autoplot(dat_pca,
-         data = dat,
+         data = dat_omit,
          colour = 'Species',
          #loadings = TRUE,
          #loadings.label = T
          )
 
 autoplot(dat_pca,
-         data = dat,
+         data = dat_omit,
          colour = 'Species',
          loadings = TRUE,
          loadings.label = T,
@@ -66,16 +66,18 @@ derivative_dat <- dat %>%
            Spikelet_shape = dat$Spikelet_length/dat$Spikelet_width,
            Leaf_packing = dat$Sheath_length/dat$Internode_dist)
 
+derivative_dat <- na.omit(derivative_dat)
 dat_pca <- prcomp(derivative_dat[,3:16], scale = T)
 
 plot(dat_pca$x[,1], dat_pca$x[,3])
 
 autoplot(dat_pca,
-         data = dat,
+         data = derivative_dat,
          colour = 'Species',
          #loadings = TRUE,
          #loadings.label = T,
-         size = 5
+         size = 5,
+         alpha(0.7)
 )
 
 autoplot(dat_pca,
@@ -144,3 +146,48 @@ autoplot(dat_pca,
          size = 5
 )
 jh
+
+ramclade <- dat %>% filter(Species == "E_rehmannii_subspicata" | 
+                               Species == "E_rehmannii_filiformis" |
+                               Species == "E_rehmannii_rehmannii" |
+                               Species == "E_ramosa_ramosa" |
+                               Species == "E_ramosa_aphylla")
+na.omit(ramclade)
+ramclade$Collection <- as.factor(ramclade$Collection)
+ramclade_pca <- prcomp(ramclade[,4:15], scale. = T)
+autoplot(ramclade_pca,
+         data = ramclade,
+         colour = 'Species',
+         #loadings = TRUE,
+         #loadings.label = T,
+         size = 5
+)
+
+
+derivative_ram <- ramclade %>%
+    select(Collection,
+           Species,
+           Plant_height,
+           Leaf_width,
+           Leaf_length,
+           Internode_dist,
+           Culm_diam,
+           Spikelet_no,
+           Pedicel_length_epu) %>%
+    mutate(Clearance = ramclade$Plant_height/ramclade$Veg_height,
+           Leaf_importance = ramclade$Leaf_length/ramclade$Sheath_length,
+           Glume_sheathing = ramclade$Glume_length_outer/ramclade$Spikelet_length,
+           Lemma_balance = ramclade$Lemma_outer_length/ramclade$Lemma_inner_length,
+           Spikelet_shape = ramclade$Spikelet_length/ramclade$Spikelet_width,
+           Leaf_packing = ramclade$Sheath_length/ramclade$Internode_dist)
+
+
+dat_pca <- prcomp(derivative_ram[,4:15], scale. = T)
+
+autoplot(dat_pca,
+         data = ramclade,
+         colour = 'Species',
+         loadings = TRUE,
+         loadings.label = T,
+         size = 5
+)
